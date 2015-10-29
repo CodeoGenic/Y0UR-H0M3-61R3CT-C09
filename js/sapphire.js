f99ce6f4e5373707983432668cd8b93bd33bbb4d
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2015-10-29 22:25:29 +0000

    Products dynamically added left, menu colour change

--- js/sapphire.js
+++ js/sapphire.js
@@ -9,15 +9,58 @@ $(document).ready(function()
 {
 	$.getJSON("items.json", function(json){
 
-		$("img[src*='image/blank.png']").each(function(index) {
-			const imgContainer = $(this).parent().parent();
-			$(this).attr("src", json.items[index].image);
-			imgContainer.find('h3').text(json.items[index].name);
-			imgContainer.find('p').text("£"+json.items[index].price);
+		//$("img[src*='image/blank.png']").each(function(index) {
+		//	const imgContainer = $(this).parent().parent();
+		//	$(this).attr("src", json.items[index].image);
+		//	imgContainer.find('h3').text(json.items[index].name);
+		//	imgContainer.find('p').text("£"+json.items[index].price);
+
+		//var div = $.innerHTML(div.id="myProducts");
+
+
+		//$("img[div]").each(function(index) {
+		//	const imgContainer = $(this).parent().parent();
+		//	$(this).attr("src", json.items[index].image);
+		//	text(json.items[index].name);
+		//	text("£"+json.items[index].price);
+
+
+			var myItems = document.getElementById("myProducts");
+			var items = json.items;
+		    var html2 = "";
+		    var p = document.createElement("p");
+			for(var i = 0; i < items.length; i++) {
+				var html =
+					'<div class = "col-md-4">' +
+					'<div class ="product">' +
+					'<a><img src= "'+items[i].image+'" class="center-block img-responsive"></a>' +
+					'<div class="name"><p class="text-center">'+items[i].name+'</p></div>' +
+					'<div class="price"><p class="text-center">£'+items[i].price+'</p></div>' +
+					'</div>' +
+					'</div>'
+
+				html2+=html;
+
+
+			}
+
+		myItems.innerHTML = html2;
+
+
+
+			//}
 
 		});
 //
 		//           $xi.parent().parent().find('p').text(data[randItems[i+3]].description.split(".")[0]);
-	})
 
 });
+
+//<div class="col-md-4">
+//<div class="product">
+//<a><img src="image/blank.png" class="center-block img-responsive"></a>
+//<h3 class="text-center">A title</h3></div>
+//<p class="text-center">From £579.99</p>
+//<button type="button" class="btn btn-danger center-block" data-toggle="modal" data-target="#exampleModal">View</button>
+//</div>
+//</div>
