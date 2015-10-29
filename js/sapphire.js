428192d6f64c21920674f04d3aa704ae47191f90
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2015-10-29 19:02:24 +0000

    JSON file created & test products added

--- js/sapphire.js
+++ js/sapphire.js
@@ -1,4 +1,23 @@
 $(window).on('load', function () 
 {
 	$('.selectpicker').selectpicker({});
+
+
+});
+
+$(document).ready(function()
+{
+	$.getJSON("items.json", function(json){
+
+		$("img[src*='image/blank.png']").each(function(index) {
+			const imgContainer = $(this).parent().parent();
+			$(this).attr("src", json.items[index].image);
+			imgContainer.find('h3').text(json.items[index].name);
+			imgContainer.find('p').text("£"+json.items[index].price);
+
+		});
+//
+		//           $xi.parent().parent().find('p').text(data[randItems[i+3]].description.split(".")[0]);
+	})
+
 });
