6bbad190f567adfc9e774b9135be9167e7fb1735
Marcus Rodney <marcus.rodney@hotmail.co.uk>
2015-11-05 16:20:09 +0000

    custom scrollbar

--- js/sapphire.js
+++ js/sapphire.js
@@ -8,6 +8,30 @@ $(window).on('load', function ()
 $(document).ready(function()
 {
 
+	//var scrollOn = ;
+
+	//var $elems = $("html, body");
+	//var delta = 0;
+    //
+	//$(document).on("mousemove", function(e) {
+	//	var h = $(window).height();
+	//	var y = e.clientY - h / 2;
+	//	delta = y * 0.1;
+	//});
+    //
+	//$(window).on("blur mouseleave", function() {
+	//	delta = 0;
+	//});
+    //
+	//(function f() {
+	//	if(delta) {
+	//		$elems.scrollTop(function(i, v) {
+	//			return v + delta;
+	//		});
+	//	}
+	//	webkitRequestAnimationFrame(f);
+	//})();
+
 
 	$.getJSON("items.json", function(json){
 
